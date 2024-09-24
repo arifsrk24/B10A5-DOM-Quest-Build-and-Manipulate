7c06116cb5e8d77f5b0b08cf093ab0981a67eb47
arifsrk24 <arif.webdeveloper24@gmail.com>
2024-09-25 03:22:46 +0600

    full assignment commit

---
+++ JavaScript/blog.js
@@ -0,0 +1,8 @@
+// document.getElementById("btn-home").addEventListener("click", function () {
+//     window.location.href = "https://almamun528.github.io/donate/";
+//   }); 
+  
+document.getElementById("btn-home").addEventListener("click", function () {
+    window.location.href = "./index.html";
+    
+  }); 
